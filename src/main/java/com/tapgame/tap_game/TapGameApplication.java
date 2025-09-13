7b1f658c5f552394edae94871ad5708389ec9045
gface <89259112970@mail.ru>
2025-09-13 14:37:43 +0300

    Initial commit

---
+++ src/main/java/com/tapgame/tap_game/TapGameApplication.java
@@ -0,0 +1,13 @@
+package com.tapgame.tap_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tapgame/tap_game/TapGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tapgame.tap_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
